2381f47714ea675640f5544c27893a1c9eb1d998
Simone Spaccarotella <simone.spaccarotella@bbc.co.uk>
2025-12-01 12:59:45 +0000

    Configured linter and prettier, integrated with lint staged

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
-import prettier from 'eslint-config-prettier/flat'
+import prettier from "eslint-config-prettier/flat";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
