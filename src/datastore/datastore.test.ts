86d029d6b07efe66bbe1deaaf7b6d1456cbff681
Simone Spaccarotella <simone.spaccarotella@bbc.co.uk>
2025-12-01 14:41:53 +0000

    Add empty test to make Jest happy for now

---
+++ src/datastore/datastore.test.ts
@@ -0,0 +1,9 @@
+/**
+ * Minimal placeholder test so Jest has at least one test to run and doesn't fail the CI.
+ */
+
+describe("datastore - placeholder", () => {
+  test("no-op placeholder", () => {
+    expect(true).toBe(true);
+  });
+});
