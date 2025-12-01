dddf4a523cacaf943c090ce23734beae18f2f9ab
Simone Spaccarotella <simone.spaccarotella@bbc.co.uk>
2025-12-01 12:50:37 +0000

    test update

--- src/myndstream.ts
+++ src/myndstream.ts
@@ -2,6 +2,8 @@
  * Example function to demonstrate testing functionality.
  */
 
+const lalla = "This is a sample string.";
+
 export function greet(name: string): string {
   if (!name) {
     return "Hello, who are you?";
